95a16f6d3e6032bae2910cfa29852c9ae26710c9
Maria2009gift <kovalenkom2009@gmail.com>
2023-06-11 20:58:32 +0300

    fix

--- main.js
+++ main.js
@@ -2,48 +2,48 @@
 
 // -----1-----
 
-// const tC = 15
-// let tF
-// console.log((tC * 1.8) + 32)
+const tC = 15
+let tF
+console.log((tC * 1.8) + 32)
 
 // -----2-----
 
-// const day = 30
-// const hours = day * 24
-// console.log("hours>>", hours)
-// const minute = console.log("minutes>>", hours * 60)
+const day = 30
+const hours = day * 24
+console.log("hours>>", hours)
+const minute = console.log("minutes>>", hours * 60)
 
 // -----3-----
 
-// const health = 20
-// const energy = 40
-// console.log("health>>", health - 5)
-// console.log("energy>>", energy - 15)
+const health = 20
+const energy = 40
+console.log("health>>", health - 5)
+console.log("energy>>", energy - 15)
 
 // -----4-----
 
-// const sum =  250
-// console.log("Вартість з знижкою>>", sum - (sum * 10 / 100))
+const sum =  250
+console.log("Вартість з знижкою>>", sum - (sum * 10 / 100))
 
 // -----5-----
 
-// const numKoma = 5.653
-// console.log(Math.floor(numKoma))
+const numKoma = 5.653
+console.log(Math.floor(numKoma))
 
 // -----6-----
 
-// const stringWithFloat = "134.82px"
-// console.log(parseFloat(stringWithFloat))
+const stringWithFloat = "134.82px"
+console.log(parseFloat(stringWithFloat))
 
 // -----7-----
 
-// const stringWithInt = "4534px"
-// console.log(parseInt(stringWithInt))
+const stringWithInt = "4534px"
+console.log(parseInt(stringWithInt))
 
 // -----8-----
 
-// const numSqrt = 144
-// console.log(Math.sqrt(numSqrt))
+const numSqrt = 144
+console.log(Math.sqrt(numSqrt))
 
 // -----9-----
 
